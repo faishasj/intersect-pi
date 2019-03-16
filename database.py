b86933c48988680ed9ab0b9819e5ad7577c29168
faishasj <faishasj@gmail.com>
2019-03-17 01:49:07 +1100

    Add get_cars method

--- database.py
+++ database.py
@@ -6,14 +6,17 @@ class Database:
     def __init__(self):
         self.data = pyrebase.initialize_app(config).database()
 
-    def add_car(self,road,lane):
-        val = self.data.child("traffic").child("street_" + road).child(lane).get().val() + 1
-        self.set_cars(road,lane,val)
+    def add_car(self, road, lane):
+        val = self.get_cars(road, lane) + 1
+        self.set_cars(road, lane, val)
+
+    def get_cars(self, road, lane):
+        return self.data.child("traffic").child("street_" + road).child(lane).get().val()
 
     def set_cars(self, road, lane, val):
         self.data.child("traffic").child("street_" + road).update({lane:val})
 
     def reset_values(self):
-        for road in ["n","e","s","w"]:
+        for road in ["n", "e", "s", "w"]:
             for lane in range(3):
                 self.data.child("traffic").child("street_" + road).update({lane: 0})
